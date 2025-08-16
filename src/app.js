3a0947bfc74ce5655ad44fdeac2848f6618bbde1
sriram.k <sriram.k@intense.in>
2025-08-16 10:17:08 +0530

    implement user model and login endpoint

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 const express = require("express");
-
-const app = express();
+const mongoose = require("mongoose");
 const connectdatabase = require("./config/database");
-const e = require("express");
+const app = express();
+const User = require("./models/user");
+
+app.use(express.json());
+
+app.post("/userlogin", async (req, res) => {
+  // creating new instance of the User model
+  const user = new User(req.body);
+  try {
+    await user.save();
+
+    res.send("updated user");
+  } catch (err) {
+    res.status(400).send("Error updating user");
+  }
+});
 
 connectdatabase()
   .then(() => {
