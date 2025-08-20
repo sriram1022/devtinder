502c922b6a5cec4bfc3100a2cef80a8ada280805
sriram.k <sriram.k@intense.in>
2025-08-20 11:08:43 +0530

    implement user login, retrieval, deletion, and update endpoints; modify user schema to remove required constraints

--- src/app.js
+++ src/app.js
@@ -9,12 +9,50 @@ app.use(express.json());
 app.post("/userlogin", async (req, res) => {
   // creating new instance of the User model
   const user = new User(req.body);
+  console.log(user);
   try {
     await user.save();
-
     res.send("updated user");
   } catch (err) {
     res.status(400).send("Error updating user");
+  }
+});
+
+app.get("/userEmail", async (req, res) => {
+  const Email = req.body.email;
+  console.log(Email);
+  try {
+    const user = await User.find({ email: Email });
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(500).send("Error retrieving user");
+  }
+});
+
+app.delete("/deleteUser", async (req, res) => {
+  il = req.body.email;
+  try {
+    const user = await User.findOneAndDelete({ email: Email });
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(500).send("Error deleting user");
+  }
+});
+
+app.patch("/updateUser", async (req, res) => {
+  const Email = req.body.email;
+  const updatedData = req.body;
+  try {
+    const user = await User.findOneAndUpdate({ email: Email }, updatedData);
+    res.send("User updated successfully");
+  } catch (err) {
+    res.status(500).send("Error updating user");
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -5,24 +5,18 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   firstName: {
     type: String,
-    required: true,
   },
   lastName: {
     type: String,
-    required: true,
   },
   email: {
     type: String,
-    required: true,
-    unique: true,
   },
   password: {
     type: String,
-    required: true,
   },
   gender: {
     type: String,
-    required: true,
   },
 });
 module.exports = mongoose.model("User", userSchema);
